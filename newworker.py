8487a59bd766044e55721f2aa9ce066c0daa7eae
IdrisTheDragon <nathaniwilliamsuk@gmail.com>
2021-12-11 17:31:21 +0000

    rework

---
+++ newworker.py
@@ -0,0 +1,83 @@
+import time
+from rpi_ws281x import *
+from patterns import *
+
+import zmq
+
+# LED strip configuration:
+LED_COUNT      = 100      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+mode = 0
+colour = 0
+colours = [ Color(255, 0, 0), Color(0, 255, 0), Color(0, 0, 255), Color(127, 127, 127) ]
+customColour = [0,0,0]
+
+def parse_update(message):
+    global mode
+    global colour
+    msg = message.split("\n")
+    for m in msg:
+        t = m.split(":")
+        if t[0] == "mode":
+            mode = int(t[1])
+            print(mode)
+        elif t[0] == "color":
+            colour = int(t[1])
+        elif t[0] == 'r':
+            customColour[0] = int(t[1])
+        elif t[0] == 'g':
+            customColour[1] = int(t[1])
+        elif t[0] == 'b':
+            customColour[2] = int(t[1])
+
+
+ 
+# Main program logic follows:
+if __name__ == '__main__':
+
+    context = zmq.Context()
+    socket = context.socket(zmq.PULL)
+    socket.bind("tcp://*:5555")
+
+ 
+    # Create NeoPixel object with appropriate configuration.
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    # Intialize the library (must be called once before other functions).
+    strip.begin()
+ 
+    print ('Press Ctrl-C to quit.')
+ 
+    try:
+ 
+        while True:
+            try:
+                #print("getting msg")
+                message = socket.recv_string(flags=zmq.NOBLOCK)
+                print(message)
+                parse_update(message)
+            except zmq.ZMQError as e:
+                #print("no msg",e)
+                pass
+
+            if mode == 0:
+                colorWipe(strip, colours[colour])
+            elif mode == 1:
+                theaterChase(strip,colours[colour])
+            elif mode == 2:
+                rainbow(strip)
+            elif mode == 3:
+                rainbowCycle(strip,wait_ms=100)
+            elif mode == 4:
+                theaterChaseRainbow(strip,wait_ms=100)
+            elif mode == 5:
+                colorWipe(strip, Color(customColour[0],customColour[1],customColour[2]))
+ 
+    except KeyboardInterrupt:
+        colorWipe(strip, Color(0,0,0), 10)
--- server.py
+++ server.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python
 
-# make sure to use eventlet and call eventlet.monkey_patch()
-import eventlet
-eventlet.monkey_patch()
-
-import random
-
-import board
-import neopixel
+import zmq
 
 from flask import Flask, render_template, request, g, session, make_response, current_app, redirect, url_for
 from flask_socketio import SocketIO, emit
@@ -18,74 +11,11 @@ app.config['SECRET_KEY'] = "secret"
 # make sure to set the async_mode as 'eventlet'
 socketio = SocketIO(app, async_mode='eventlet')
 
-worker = None
-
-red = (50,0,0)
-green = (0,50,0)
-blue = (0,0,50)
-colours = [red,green,blue]
-
-class Worker(object):
-
-    worker = False
-    switch = False
-    unit_of_work = 0
-    speed = 0.2
-    mode = 0
-    r=50
-    g=0
-    b=0
-
-    def __init__(self, socketio):
-        """
-        assign socketio object to emit
-        """
-        self.socketio = socketio
-        self.pixels = neopixel.NeoPixel(board.D18, 100)
-
-    def do_work(self):
-        """
-        do work and emit message
-        """
-        # worker already running
-        if self.worker:
-           return
-        self.worker = True
-        self.switch = True
-        while self.worker:
-          if self.switch:
-            self.unit_of_work += 1
-            # must call emit from the socket io
-            # must specify the namespace
-            # self.socketio.emit("update", {"msg": self.unit_of_work}, namespace="/work")
-            if self.mode == 0:
-               self.pixels[self.unit_of_work] = (self.r,self.g,self.b)
-            elif self.mode == 1:
-               c = random.choice(colours)
-               for x in range(self.unit_of_work,(self.unit_of_work+5)):
-                 if x < 100:
-                   self.pixels[x] = c
-            else:
-               self.pixels[self.unit_of_work] = 0
-            # important to use eventlet's sleep method
-            if self.unit_of_work == 99:
-              self.unit_of_work = 0
-          eventlet.sleep(self.speed)
-    def update_pattern(self,data):
-        self.r = data['r']
-        self.g = data['g']
-        self.b = data['b']
-        self.mode = int(data['mode'])
-
-    def stop(self):
-        """
-        stop the loop
-        """
-        self.switch = False
+zmqContext = zmq.Context()
+zmqSocket = context.socket(zmq.PUSH)
+zmqSocket.connect("tcp://localhost:5555")
 
 
-    def start(self):
-        self.switch = True
 
 @app.route('/')
 def index():
@@ -93,13 +23,6 @@ def index():
     renders demo.html
     """
     return render_template('demo.html')
-
-@app.before_first_request
-def start_worker():
-    global worker
-    worker = Worker(socketio)
-    socketio.start_background_task(target=worker.do_work)
-    print("execute before server starts")
 
 
 @socketio.on('connect', namespace='/work')
@@ -110,31 +33,11 @@ def connect():
     emit("re_connect", {"msg": "connected"})
 
 
-@socketio.on('start', namespace='/work')
-def start_work():
-    """
-    trigger background thread
-    """
-    global worker
-    worker.start()
-    print("start once")
-    emit("update", {"msg": "starting worker"})
-
-
-@socketio.on('stop', namespace='/work')
-def stop_work():
-    """
-    trigger background thread
-    """
-    global worker
-    worker.stop()
-    print("stop once")
-    emit("update", {"msg": "worker has been stoppped"})
-
 @socketio.on('update', namespace='/work')
 def update_pattern(data):
-    global worker
-    worker.update_pattern(data)
+    # data['r']
+
+    zmqSocket.send_string(f"mode:{data['mode']}\ncolor:{data['colour']}\nr:{data['r']}\ng:{date['g']}\nb:{date['b']}")
     print('updated with', data)
     emit("update", {"msg":"updated data"})
 
